eeb4cdd1592e08f850419011733a3c012e0f4e1e
Veronika Vokounova <veronika.vokounova@cofis.cz>
2025-11-26 21:01:50 +0100

    configurace

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,13 +23,22 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
+  timeout: 60000,
+  globalTimeout: 1 * 60 * 60 * 1000,
+  expect: {
+    timeout: 7000,
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    actionTimeout: 7000,
+    navigationTimeout: 30000,
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    screenshot: "on",
+    video: "on",
+    trace: "on",
   },
 
   /* Configure projects for major browsers */
