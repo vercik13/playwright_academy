88dfc6f9f5ebbace2eab4f22eced6ff88c079fef
Veronika Vokounova <veronika.vokounova@cofis.cz>
2025-12-08 20:47:38 +0100

    add ddt test

---
+++ assets/dictionaries/dictionary.ts
@@ -0,0 +1,15 @@
+// assets/dictionaries/
+// dictionary.ts
+
+export const pmtoolTexts = {
+  login: {
+    title: "Login",
+    usernamePlaceholder: "Username",
+    rememberMe: "Remember Me",
+    lostPassword: "Password Forgotten?",
+    loginButton: "Login",
+  },
+  dashboard: {},
+  projects: {},
+  general: {},
+};
